77e00cad332bd10476a7981d8441dc43ee86a4e6
sloev <johannes.valbjorn@gmail.com>
2019-09-04 10:24:36 +0200

    add some spacing around the "art" :-)

--- draw.py
+++ draw.py
@@ -8,12 +8,12 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def create_canvas_render(width, height,x,y, filename="output", font_size=5, dry=False):
+def create_canvas_render(width, height, x, y, font_size, filename="output", dry=False):
     TURNS = {-1: -math.pi / 2, 0: 0, 1: math.pi / 2}
     if dry:
         device = io.BytesIO()
     else:
-        device = open(filename+'.svg', "wb")
+        device = open(filename + ".svg", "wb")
 
     surface = cairo.SVGSurface(device, width, height)
 
@@ -23,14 +23,14 @@ def create_canvas_render(width, height,x,y, filename="output", font_size=5, dry=
     context.fill()
     context.set_source_rgb(0, 0, 0)
 
-    context.select_font_face(
-        "times", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL
-    )
+    context.select_font_face("times", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
     context.set_font_size(font_size)
 
-    context.move_to(x,y)
+    context.move_to(x, y)
 
-    data = dict(direction=0, points=[context.user_to_device(*context.get_current_point())])
+    data = dict(
+        direction=0, points=[context.user_to_device(*context.get_current_point())]
+    )
 
     def render(text, turn=0):
         data["direction"] += turn
@@ -46,14 +46,12 @@ def create_canvas_render(width, height,x,y, filename="output", font_size=5, dry=
         data["points"].append(context.user_to_device(*context.get_current_point()))
 
     if dry:
-        yield render, data['points']
+        yield render, data["points"]
     else:
         yield render
-
 
     context.fill()
     context.stroke()
 
-    
     surface.finish()
     device.close()
--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from afinn import Afinn
 from tqdm import tqdm
 
 afinn = Afinn(language="en")
-
+fontsize = 5
 with open(sys.argv[1]) as f:
     width = 1000000
     height = 1000000
-    x = width/2
-    y = height/2
-    with draw.create_canvas_render(width, height, x, y, dry=True) as (render, points):
+    x = width / 2
+    y = height / 2
+    with draw.create_canvas_render(width, height, x, y, fontsize, dry=True) as (
+        render,
+        points,
+    ):
         for word in tqdm(f.read().split(" ")):
             polarity = max(min(int(afinn.score(word)), 1), -1)
             render(f" {word} ", polarity)
@@ -21,15 +24,17 @@ with open(sys.argv[1]) as f:
         x_range = x_points[0][0], x_points[-1][0]
         y_range = y_points[0][1], y_points[-1][1]
 
-    width = x_range[1] - x_range[0]
-    height = y_range[1] - y_range[0]
+    width = x_range[1] - x_range[0] + (fontsize * 11)
+    height = y_range[1] - y_range[0] + (fontsize * 11)
 
     x -= x_range[0]
+    x += fontsize * 5
     y -= y_range[0]
+    y += fontsize * 5
 
     print(f"running for real with x:{x}, y:{y}, width:{width}, height:{height}")
     f.seek(0)
-    with draw.create_canvas_render(width, height, x, y) as render:
+    with draw.create_canvas_render(width, height, x, y, fontsize) as render:
         for word in tqdm(f.read().split(" ")):
             polarity = max(min(int(afinn.score(word)), 1), -1)
             render(f" {word} ", polarity)
